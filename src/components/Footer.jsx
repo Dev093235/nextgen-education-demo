d39b22dff04754e2f760c821572c22a7dfa87ce2
Dev093235 <dev431140@gmail.com>
2026-06-21 11:11:02 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#0f172a",
+        color: "#fff",
+        textAlign: "center",
+        padding: "35px"
+      }}
+    >
+      <h2>NextGen Education</h2>
+
+      <p style={{ marginTop: "10px" }}>
+        Learn Today. Lead Tomorrow.
+      </p>
+
+      <p style={{ marginTop: "20px", color: "#94a3b8" }}>
+        © 2026 All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
