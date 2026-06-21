6e56aff74f8f7da8ab069e6669dbfb3c56e25a0f
Dev093235 <dev431140@gmail.com>
2026-06-21 11:01:50 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,11 @@
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 function Home() {
   return (
     <>
       <Navbar />
-
-      <section
-        style={{
-          textAlign: "center",
-          padding: "100px 20px"
-        }}
-      >
-        <h1
-          style={{
-            fontSize: "52px",
-            color: "#1e293b"
-          }}
-        >
-          Empowering Students
-        </h1>
-
-        <p
-          style={{
-            marginTop: "20px",
-            color: "#64748b",
-            fontSize: "18px"
-          }}
-        >
-          Learn from the best teachers and build your future with NextGen Education.
-        </p>
-
-        <button
-          style={{
-            marginTop: "30px",
-            background: "#2563eb",
-            color: "#fff",
-            padding: "14px 28px",
-            borderRadius: "8px"
-          }}
-        >
-          Explore Courses
-        </button>
-      </section>
+      <Hero />
     </>
   );
 }
