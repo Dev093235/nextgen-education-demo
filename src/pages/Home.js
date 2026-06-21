71279cc83ab481a7703cf4a6d2fce109c99edc20
Dev093235 <dev431140@gmail.com>
2026-06-21 11:07:24 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Stats from "../components/Stats";
+import Courses from "../components/Courses";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Navbar />
       <Hero />
       <Stats />
+      <Courses />
     </>
   );
 }
